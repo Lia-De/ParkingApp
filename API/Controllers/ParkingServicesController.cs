f0d9a965bd4037607df3b9f91237d067be78e444
Jenny Evertsson <jentap@gmail.com>
2025-03-11 14:01:45 +0100

    Starting a refactoring to use controllers, and return objects, not strings

--- API/Controllers/ParkingServicesController.cs
+++ API/Controllers/ParkingServicesController.cs
@@ -3,11 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel;
 using API.Services;
 using API.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace API.Controllers;
 [ApiController]
-public class ParkingServicesController : Controller
+public class ParkingServicesController : ControllerBase
 {
-
+    private ParkingServices _myParkingLot;
+    public ParkingServicesController(ParkingServices myParkingLot)
+    {
+        _myParkingLot = myParkingLot;
+    }
     [HttpPost("addUser")]
     public IActionResult AddUser([FromForm] string username, [FromForm] string password, [FromForm] string email, [FromForm] string? licensePlate)
     {
@@ -17,13 +22,12 @@ public class ParkingServicesController : Controller
             return BadRequest("Please fill in all required fields");
         }
 
-        ParkingServices myParkingLot = new ParkingServices();
 
         string carPlate = string.IsNullOrWhiteSpace(licensePlate) ? "" : licensePlate.ToUpper();
-        myParkingLot.RegisterUser(username, password, email, carPlate);
+        _myParkingLot.RegisterUser(username, password, email, carPlate);
 
         string feedback = $"User {username} added to the system\n";
-        feedback += myParkingLot.Report();
+        feedback += _myParkingLot.Report();
         return Ok($"{feedback}");
     }
 
@@ -36,13 +40,13 @@ public class ParkingServicesController : Controller
             return BadRequest("Please fill in all required fields");
         }
 
-        ParkingServices myParkingLot = new ParkingServices();
+        
 
         string carPlate = carPlate = string.IsNullOrWhiteSpace(licensePlate) ? "" : licensePlate.ToUpper();
         try
         {
-            ParkingUser user = myParkingLot.ParkingUsers.FirstOrDefault(u => u.Id == userID);
-            myParkingLot.RegisterCar(userID, carPlate);
+            ParkingUser user = _myParkingLot.ParkingUsers.FirstOrDefault(u => u.Id == userID);
+            _myParkingLot.RegisterCar(userID, carPlate);
 
             string feedback = $"User {user.UserName} added new car {carPlate} to the system\n";
 
@@ -52,6 +56,26 @@ public class ParkingServicesController : Controller
         {
             return BadRequest(ex.Message);
         }
+    }
+
+    [HttpGet("user/{userId}")]
+    public ParkingUser? GetSingleUser(int userId)
+    {
+        ParkingUser? user = _myParkingLot.ParkingUsers.SingleOrDefault(u => u.Id == userId);
+        return user;
+        //    if (user == null) return $"No such user exists";
+        //    string feedback = "";
+        //    int carCount = user.Cars.Count;
+        //    feedback += $"User {user.UserName} ({user.Email}) has {carCount} cars registered and currently owes {user.ParkingFeesOwed:F2} SEK\n";
+        //    if (carCount > 0) {
+        //        feedback += $"Cars belonging to {user.UserName} are: ";
+        //        foreach (var car in user.Cars)
+        //        {
+        //            feedback += car.ToString() + ", ";
+        //        }
+        //    }
+        //    return feedback.Trim(' ', ',');
+
     }
 
 }
