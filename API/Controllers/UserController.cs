edcd01312ff77f7a471f10024877c2d94f80b0ce
Jenny Evertsson <jentap@gmail.com>
2025-03-19 16:09:46 +0100

    Adding a login functionality

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using API.Services;
+using API.Models;
+namespace API.Controllers;
+
+public class UserController : ControllerBase
+{
+    private ParkingServices _myParkingLot;
+    public UserController(ParkingServices myParkingLot)
+    {
+        _myParkingLot = myParkingLot;
+    }
+
+    [HttpPost("/user/login")]
+    public IActionResult Login([FromBody] CreateUserDTO newUser)
+    {
+
+        if (newUser.UserName is null || newUser.UserName.Length < 1)
+        {
+            return BadRequest("BACKEND Please fill in all required fields");
+        }
+        ParkingUser? loggedInUser = _myParkingLot.ParkingUsers.FirstOrDefault(u => u.UserName == newUser.UserName);
+        if (loggedInUser is null)
+        {
+            return BadRequest("User not found");
+        }
+        if (loggedInUser.Password != newUser.Password)
+        {
+            return BadRequest("Incorrect password");
+        }
+        return Ok(loggedInUser.Id);
+
+    }
+}
